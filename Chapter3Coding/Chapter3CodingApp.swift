40bcc4f0fb655b16f60e6f5d54fa6d1c20db84ca
Elham <elham.farhan@gmail.com>
2023-11-14 16:23:47 -0500

    Initial Commit

---
+++ Chapter3Coding/Chapter3CodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter3CodingApp.swift
+//  Chapter3Coding
+//
+//  Created by Elham Farhan on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Chapter3CodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
