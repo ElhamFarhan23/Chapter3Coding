e97b31eff101b6ea069a47282b5b5d253393bfe8
Elham <elham.farhan@gmail.com>
2023-11-17 13:11:39 -0500

    updated

---
+++ Chapter3Coding/sliders.swift
@@ -0,0 +1,18 @@
+//
+//  sliders.swift
+//  Chapter3Coding
+//
+//  Created by Elham Farhan on 11/17/23.
+//
+
+import SwiftUI
+
+struct sliders: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    sliders()
+}
